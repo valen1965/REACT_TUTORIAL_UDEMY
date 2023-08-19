12f97f09cdd209c791496ac8fbe61aa1bea92cf0
Vlad Vazhnenko <vladvazhnenko@gmail.com>
2023-08-20 00:26:12 +0300

    React fundamentals

--- src/index.js
+++ src/index.js
@@ -1,10 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
-function Greeting() {
-  return <h2>My First Component</h2>;
-}
+const BookList = () => {
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+};
+
+const Book = () => {
+  return (
+    <article className="book">
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => (
+  <img
+    src="https://images-na.ssl-images-amazon.com/images/I/91PU1YVJm5L._AC_UL600_SR600,400_.jpg"
+    alt="Single AF Cocktails"
+  />
+);
+const Title = () => <h2>Single AF Cocktails</h2>;
+const Author = () => {
+  return <h4>Ariana Madix</h4>;
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Greeting />);
+root.render(<BookList />);
